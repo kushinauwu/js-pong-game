299fd1066a8fdfc97f53e9410956716c5215eb22
Deepashree Deshmukh <18piscean@gmail.com>
2019-03-16 20:57:08 +0530

    Update pong.js

--- pong.js
+++ pong.js
@@ -38,47 +38,52 @@ class Ball extends Rectangle {
     }
 }
 
-// crate pong canvas and elements
+class Pong {
+    constructor(canvas) {
+        this._canvas = canvas;
+        this._context = canvas.getContext('2d');
+
+        this.ball = new Ball;
+        console.log(this.ball);
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 100;
+        this.ball.velocity.x = 100;
+        this.ball.velocity.y = 100;
+
+        let lastTime;
+
+        // use requestAnimationFrame to call callback to update the ball's position using arrow function
+        const callback = (millis) => {
+            // if a lastTime exists, update its position 
+            if (lastTime) {
+                this.updatePosition((millis - lastTime) / 1000);
+            }
+            lastTime = millis;
+            requestAnimationFrame(callback);
+        }
+        callback();
+    }
+
+    updatePosition(dt) {
+        this.ball.pos.x += this.ball.velocity.x * dt;
+        this.ball.pos.y += this.ball.velocity.y * dt;
+
+        if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
+            this.ball.velocity.x = -this.ball.velocity.x
+        }
+
+        if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            this.ball.velocity.y = -this.ball.velocity.y
+        }
+
+        this._context.fillStyle = 'black';
+        this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+
+        this._context.fillStyle = 'white';
+        this._context.fillRect(this.ball.pos.x, this.ball.pos.y, this.ball.size.x, this.ball.size.y);
+    }
+}
+
+// create pong canvas and elements
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
-
-const ball = new Ball;
-console.log(ball);
-ball.pos.x = 100;
-ball.pos.y = 100;
-ball.velocity.x = 50;
-ball.velocity.y = 50;
-
-let lastTime;
-
-// use requestAnimationFrame to call callback to update the ball's position
-function callback(millis) {
-    // if a lastTime exists, update its position 
-    if (lastTime) {
-        updatePosition((millis - lastTime) / 1000);
-    }
-    lastTime = millis;
-    requestAnimationFrame(callback);
-}
-
-// update position of pong elements
-function updatePosition(dt) {
-    ball.pos.x += ball.velocity.x * dt;
-    ball.pos.y += ball.velocity.y * dt;
-
-    if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
-        ball.velocity.x = -ball.velocity.x
-    }
-
-    if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
-        ball.velocity.y = -ball.velocity.y
-    }
-
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.fillStyle = 'white';
-    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-}
-
-callback();
+const pong = new Pong(canvas);
