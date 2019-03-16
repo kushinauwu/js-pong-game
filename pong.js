a845f6256b0284af7c20ebb185e63ed964ef8efd
Deepashree Deshmukh <18piscean@gmail.com>
2019-03-16 20:19:50 +0530

    Update pong.js

--- pong.js
+++ pong.js
@@ -14,7 +14,7 @@ class Rectangle {
     }
 }
 
-//Ball inherits from rectangle
+// Ball inherits from rectangle
 class Ball extends Rectangle {
     constructor() {
         super(10, 10);
@@ -22,14 +22,39 @@ class Ball extends Rectangle {
     }
 }
 
+// crate pong canvas and elements
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
 const ball = new Ball;
 console.log(ball);
+ball.pos.x = 100;
+ball.pos.y = 100;
+ball.velocity.x = 50;
+ball.velocity.y = 50;
 
-context.fillStyle = 'black';
-context.fillRect(0, 0, canvas.width, canvas.height);
+let lastTime;
 
-context.fillStyle = 'white';
-context.fillRect(0, 0, 10, 10);
+// use requestAnimationFrame to call callback to update the ball's position
+function callback(millis) {
+    // if a lastTime exists, update its position 
+    if (lastTime) {
+        updatePosition((millis - lastTime) / 1000);
+    }
+    lastTime = millis;
+    requestAnimationFrame(callback);
+}
+
+// update position of pong elements
+function updatePosition(dt) {
+    ball.pos.x += ball.velocity.x * dt;
+    ball.pos.y += ball.velocity.y * dt;
+
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = 'white';
+    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+}
+
+callback();
